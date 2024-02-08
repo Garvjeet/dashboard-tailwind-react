f13474fb19b39d455e9a8158ea85958f232c4996
Garvjeet Singh <singh.garvjeet@gmail.com>
2024-02-08 15:48:02 +0530

    removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,6 @@ import { RightPanel } from './components/RightPanel';
 function App() {
   return (
     <div className="flex">
-      {/* <RevenueCard
-        title="Title"
-        orderCount="4"
-        count={4}
-        amount={45988}
-      /> */}
-      {/* <div className='bg-red-500 col-span-3 md:col-span-1 text-left sm:text-right'>hi1</div>
-      <div className='bg-yellow-500 col-span-3 md:col-span-1'>hi2</div>
-      <div className='bg-green-500 col-span-3 md:col-span-1'>hi3</div> */}
-      {/* <RevenueCard></RevenueCard> */}
       <SideBar />
       <RightPanel />
     </div>
